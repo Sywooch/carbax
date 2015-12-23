b6cb0e71dbcbd54eb2198d8267f22d1005aa854d
Kirill <apuc06@mail.ru>
2015-12-23 15:42:38 +0300

    search filter

---
+++ frontend/widgets/CheckBoxAutoZap.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Кирилл
+ * Date: 23.12.2015
+ * Time: 14:39
+ */
+
+namespace frontend\widgets;
+
+
+use yii\base\Widget;
+
+class CheckBoxAutoZap extends Widget
+{
+
+    public function run(){
+        
+    }
+
+}
