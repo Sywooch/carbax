922d94974a8dcc2df824385b5922c0fb281c78d2
king199025 <korol_dima@list.ru>
2015-12-24 16:06:19 +0300

    спецпредложения таблицей

--- frontend/modules/offers/views/offers/index.php
+++ frontend/modules/offers/views/offers/index.php
@@ -11,36 +11,22 @@ $this->registerCssFile('/css/bootstrap.min.css');
 <section class="main-container">
     <div class="offers_page">
         <div class="offers__offers-list">
-            <?php
-            $i = 0;
-            foreach ($models as $n):?>
-                <?php if ($i == 0): ?>
-                    <div class="offers__row filter__container">
-                <?php endif ?>
-                <div class="offers__block">
-                    <img src="<?= $n->img_url ?>" alt="">
-                    <a href="<?= Url::to(['offers/view', 'id' => $n->id]) ?>"
-                       class="offers__block-title"><?= $n->title ?></a><br /><br />
-                    <a class="btn btn-default btn-xs" href="<?= Url::to(['offers/edit', 'id' => $n->id]); ?>">Редактировать</a>
-                    <br />
-                    <br />
-                    <a class="btn btn-danger btn-xs" href="<?= Url::to(['offers/delete', 'id' => $n->id]); ?>">Удалить</a>
-                </div>
-
-                <?php
-                $i = $i + 1;
-                if ($i == 2):
-                    $i = 0;
-                    ?>
-                    </div>
-                <?php endif ?>
-
-                <?php
-            endforeach;
-            ?>
-            <?php if ($i != 0): ?>
-        </div>
-        <?php endif ?>
+            <table class="table table-condensed">
+                <tr>
+                    <th>Изображение</th>
+                    <th>Название</th>
+                    <th></th>
+                </tr>
+            <?php foreach ($models as $n): ?>
+                <tr>
+                    <td> <div class="offers__block"><img src="<?= $n->img_url ?>" alt=""></div></td>
+                    <td><a href="<?= Url::to(['offers/view', 'id' => $n->id]) ?>" class="offers__block-title"><?= $n->title ?></a></td>
+                    <td><a class="btn btn-default btn-xs" href="<?= Url::to(['offers/edit', 'id' => $n->id]); ?>">Редактировать</a>
+                        <a class="btn btn-danger btn-xs" href="<?= Url::to(['offers/delete', 'id' => $n->id]); ?>">Удалить</a></td>
+                </tr>
+            <?php endforeach; ?>
+            </table>
+     
         <?php
         // display pagination
         echo LinkPager::widget(['pagination' => $pages]); ?>
