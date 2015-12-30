ec661927af4a80819e2a69eff9fad754354f1a42
king199025 <korol_dima@list.ru>
2015-12-30 11:25:30 +0300

    select brand1

--- frontend/widgets/views/sel_mult_moto.php
+++ frontend/widgets/views/sel_mult_moto.php
@@ -2,5 +2,5 @@
 use yii\helpers\Html;
 ?>
     <h3>Выберите марки мото транспорта с которыми вы работаете</h3>
-<?= Html::dropDownList('brandsMoto',$selMark,$mark,['id'=>'markType','class'=>'form-control','multiple'=>'multiple']); ?>
+<?= Html::dropDownList('brandsMoto',$selMark,$mark,['id'=>'markType','class'=>'form-control','multiple'=>'multiple','size'=>'5']); ?>
 <span class="mult_info">Для выбора нескольких, при клике, нажмите клавиш CTRL на клавиатуре</span>
