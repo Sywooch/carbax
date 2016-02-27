899d6fdcfff8abdc9dcfa1444ab29ab907b0670e
AKD47 <van83horn@gmail.com>
2016-02-27 13:30:22 +0300

    спецпредложение начало

--- frontend/modules/offers/views/offers/view.php
+++ frontend/modules/offers/views/offers/view.php
@@ -38,8 +38,138 @@ $IpGeoBase->updateDB()
 
 
 <!--AKD47 section-->
+<h3 class="offers_header">Комплексная химчистка салона автомобиля в сети сервисных центров “На колесах.ru”</h3>
 <section class="main-container">
     <div class="offers_page_view">
+
+        <div class="offers_main--img">
+            <img class="main__img" src="../../media/img/request1.png" alt=""/>
+        </div>
+
+        <div class="offers_side--img">
+            <div class="side__img">
+                <img class="side__img--sm" src="../../media/img/request2.png" alt=""/>
+            </div>
+            <div class="side__img">
+                <img class="side__img--sm" src="../../media/img/request2.png" alt=""/>
+            </div>
+
+            <div class="side__img">
+                <img class="side__img--sm" src="../../media/img/request2.png" alt=""/>
+            </div>
+
+            <div class="side__img">
+                <img class="side__img--sm" src="../../media/img/request2.png" alt=""/>
+            </div>
+
+        </div>
+        <div class="offers_nav">
+            <ul class="nav_sm nav nav-tabs">
+                <li><a href="#conditions" role="tab" data-toggle="tab">Условия</a></li>
+                <li><a href="#description" role="tab" data-toggle="tab">Описание</a></li>
+                <li><a href="#reviews" role="tab" data-toggle="tab">Отзывы (125)</a></li>
+            </ul>
+        </div>
+        <div class="tab-content">
+            <div role="tabpanel" class="tab-pane fade in active" id="conditions">
+                <div class="offers_page_view">
+                    <div class="row">
+
+                      <p class="offers_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                          laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                          voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                          non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                          unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                          magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+                          ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+                          incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+                        <p class="offers_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                            unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+                            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+                            incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+                    </div>
+
+
+                    <span class="question"><a class="ask" href="#">Задать вопрос</a></span>
+                    
+                    
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="description">
+                <div class="offers_page_view">
+                    <div class="row">
+
+                        <p class="offers_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                            unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+                            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+                            incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+                        <p class="offers_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                            unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+                            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+                            incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+                    </div>
+
+
+                    <span class="question"><a class="ask" href="#">Задать вопрос</a></span>
+
+
+                </div>
+            </div>
+            <div role="tabpanel" class="tab-pane fade" id="reviews">
+                <div class="offers_page_view">
+                    <div class="row">
+
+                        <p class="offers_content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
+                            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+                            laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
+                            voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+                            non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Sed ut perspiciatis
+                            unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+                            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.
+                            Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur
+                            magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem
+                            ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora
+                            incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+
+
+                    </div>
+
+
+
+
+                </div>
+            </div>
+
+        </div>
+
 
     </div>
 </section>
