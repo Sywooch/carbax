c3d39a3bc2faba7e4155b613fb5afffaabd52349
AKD47 <van83horn@gmail.com>
2016-03-11 10:18:55 +0300

    автосалоны конец

--- frontend/modules/services/views/services/view.php
+++ frontend/modules/services/views/services/view.php
@@ -220,32 +220,48 @@ $this->registerCssFile('/css/bootstrap_btn.min.css');
             </div>
 
             <div class="comfortZone">
-                <img src="/frontend/web/media/img/wifi-1.png">
+                <img src="/frontend/web/media/img/wifi-1.png"><span>Wi-Fi</span>
             </div>
 
             <div class="comfortZone">
-                <img src="/frontend/web/media/img/shiny.png">
+                <img src="/frontend/web/media/img/shiny.png"><span>Подкачка шин</span>
             </div>
 
         </div>
         <div class="singleContent__worksWith-block">
 
             <div class="comfortZone">
-                <img src="/frontend/web/media/img/coffemachine.png">
+                <img src="/frontend/web/media/img/coffemachine.png"><span>Кофе-автомат</span>
             </div>
 
             <div class="comfortZone">
-                <img src="/frontend/web/media/img/payment.png">
+                <img src="/frontend/web/media/img/payment.png"><span>Оплата картой</span>
             </div>
 
             <div class="comfortZone">
-                <img src="/frontend/web/media/img/terminal.png">
+                <img src="/frontend/web/media/img/terminal.png"><span>Терминал оплаты</span>
             </div>
 
         </div>
 
     </div>
 
+    <div class="singleContent__worksWith">
+        <h3>Работаем с:</h3>
+        <div class="singleContent__worksWith-block">
+
+            <h4>С грузовыми авто</h4>
+            <img src="/media/img/gruz.png" alt="">
+        </div>
+
+
+        <div class="singleContent__worksWith-block">
+
+            <h4>С легковыми авто</h4>
+            <img src="/media/img/sedan.png" alt="">
+        </div>
+
+    </div>
 
 
 </section>
@@ -253,112 +269,112 @@ $this->registerCssFile('/css/bootstrap_btn.min.css');
 
 
 
-<section class="main-container">
-            <h3>Название автосервиса:</h3><?=$serviceName;?>
-            <div class="logoService">
-            <?php
-                if(!empty($serviceLogo)){
-                    echo Html::img('/'.$serviceLogo,['width'=>'100px','alt'=>$serviceName]);
-                }
-            ?>
-            </div>
-            <div class="singleContent__desc">
-                <h3>Описание</h3>
-                    <div>
-                        <?=$description;?>
-                    </div>
-                <h3>Адрес</h3>
-                <div class="serviceAddress">
-                    <?php
-                        foreach($address as $ad){
-                            ?>
-                            <div class="address"><?=$ad->address;?></div>
-                        <?php
-                        }
-                    ?>
-                </div>
-                <h3>Время работы</h3>
-                <div>
-                    <?php
-                        $custFunc = new Custom_function();
-                        foreach($workHours as $wh):
-
-                    ?>
-                        <div>
-                            <span><?=$custFunc->get_week_day($wh->day)?></span> - <?php
-                            if($wh->{'24h'} == 1){ ?>
-                                Круглосуточно
-                            <?php
-                            }
-                            else{
-                                ?>
-                                С <?=$wh->hours_from?> ДО <?=$wh->hours_to?>
-                            <?php
-                            }?>
-                        </div>
-                    <?php
-                        endforeach;
-                    ?>
-                </div>
-                <div class="singleContent__desc--contacts">
-                    <h3>Контакты</h3>
-                    <div class="singleContent__desc--line">
-                        <label for="website">Web-сайт</label>
-                        <?=$website?>
-                    </div>
-                    <div class="singleContent__desc--line">
-                        <label for="phonenumber">Телефоны</label>
-                        <?php
-                            foreach($phone as $phon):
-                        ?>
-                                <div><?=$phon->number;?></div>
-                        <?php
-                            endforeach;
-                        ?>
-
-                    </div>
-                    <div class="singleContent__desc--line">
-                        <label for="mailadress">Почта</label>
-                       <span><?=$email;?></span>
-
-                    </div>
-                </div>
-
-
-                 <?php
-                   echo PrintAdditionalFieldsByServisId::widget(['servicId'=>$serviceID])
-                 ?>
-               <!-- <div class="singleContent__desc--carbrands">
-                    <h3>Марки автомобилей</h3>
-                    <?php /*foreach($carBrands as $cb):*/?>
-                        <div class="carBrands"><?/*= BcbBrands::find()->where(['id'=>$cb->brand_cars_id])->one()->name; */?></div>
-                    <?php /*endforeach;*/?>
-                </div>-->
-
-                <div class="singleContent__services">
-                    <div class="singleContent__services-comfort">
-                        <h3>Зоны комфорта</h3>
-                        <?php foreach($comfortZone as $cz):?>
-                            <div class="comfortZone">
-                                <img src="<?=$cz->img_ulr;?>"/><span><?=$cz->name;?></span>
-                            </div>
-                        <?php endforeach;?>
-
-                    </div>
-                </div>
-                <?php if($serviceType->view_widget_auto_type == 1 ): ?>
-                    <div class="singleContent__worksWith">
-                        <h3>Работаем</h3>
-                       <!-- <?php /*foreach($autoType as $at):*/?>
-                            <div class="singleContent__worksWith-block">
-
-                                <h4><?/*=$at->name*/?></h4>
-                                <img src="<?/*=$at->img_url;*/?>" alt="">
-                            </div>
-                        --><?php /*endforeach;*/?>
-
-                        <?= AutoType::widget(['view'=>1,'serviceId'=>$serviceID])?>
-                    </div>
-                <?php endif; ?>
-            </div>
-</section>
+<!--<section class="main-container">-->
+<!--            <h3>Название автосервиса:</h3>--><?//=$serviceName;?>
+<!--            <div class="logoService">-->
+<!--            --><?php
+//                if(!empty($serviceLogo)){
+//                    echo Html::img('/'.$serviceLogo,['width'=>'100px','alt'=>$serviceName]);
+//                }
+//            ?>
+<!--            </div>-->
+<!--            <div class="singleContent__desc">-->
+<!--                <h3>Описание</h3>-->
+<!--                    <div>-->
+<!--                        --><?//=$description;?>
+<!--                    </div>-->
+<!--                <h3>Адрес</h3>-->
+<!--                <div class="serviceAddress">-->
+<!--                    --><?php
+//                        foreach($address as $ad){
+//                            ?>
+<!--                            <div class="address">--><?//=$ad->address;?><!--</div>-->
+<!--                        --><?php
+//                        }
+//                    ?>
+<!--                </div>-->
+<!--                <h3>Время работы</h3>-->
+<!--                <div>-->
+<!--                    --><?php
+//                        $custFunc = new Custom_function();
+//                        foreach($workHours as $wh):
+//
+//                    ?>
+<!--                        <div>-->
+<!--                            <span>--><?//=$custFunc->get_week_day($wh->day)?><!--</span> - --><?php
+//                            if($wh->{'24h'} == 1){ ?>
+<!--                                Круглосуточно-->
+<!--                            --><?php
+//                            }
+//                            else{
+//                                ?>
+<!--                                С --><?//=$wh->hours_from?><!-- ДО --><?//=$wh->hours_to?>
+<!--                            --><?php
+//                            }?>
+<!--                        </div>-->
+<!--                    --><?php
+//                        endforeach;
+//                    ?>
+<!--                </div>-->
+<!--                <div class="singleContent__desc--contacts">-->
+<!--                    <h3>Контакты</h3>-->
+<!--                    <div class="singleContent__desc--line">-->
+<!--                        <label for="website">Web-сайт</label>-->
+<!--                        --><?//=$website?>
+<!--                    </div>-->
+<!--                    <div class="singleContent__desc--line">-->
+<!--                        <label for="phonenumber">Телефоны</label>-->
+<!--                        --><?php
+//                            foreach($phone as $phon):
+//                        ?>
+<!--                                <div>--><?//=$phon->number;?><!--</div>-->
+<!--                        --><?php
+//                            endforeach;
+//                        ?>
+<!---->
+<!--                    </div>-->
+<!--                    <div class="singleContent__desc--line">-->
+<!--                        <label for="mailadress">Почта</label>-->
+<!--                       <span>--><?//=$email;?><!--</span>-->
+<!---->
+<!--                    </div>-->
+<!--                </div>-->
+<!---->
+<!---->
+<!--                 --><?php
+//                   echo PrintAdditionalFieldsByServisId::widget(['servicId'=>$serviceID])
+//                 ?>
+<!--               <!-- <div class="singleContent__desc--carbrands">-->
+<!--                    <h3>Марки автомобилей</h3>-->
+<!--                    --><?php ///*foreach($carBrands as $cb):*/?>
+<!--                        <div class="carBrands">--><?///*= BcbBrands::find()->where(['id'=>$cb->brand_cars_id])->one()->name; */?><!--</div>-->
+<!--                    --><?php ///*endforeach;*/?>
+<!--                </div>-->
+<!---->
+<!--                <div class="singleContent__services">-->
+<!--                    <div class="singleContent__services-comfort">-->
+<!--                        <h3>Зоны комфорта</h3>-->
+<!--                        --><?php //foreach($comfortZone as $cz):?>
+<!--                            <div class="comfortZone">-->
+<!--                                <img src="--><?//=$cz->img_ulr;?><!--"/><span>--><?//=$cz->name;?><!--</span>-->
+<!--                            </div>-->
+<!--                        --><?php //endforeach;?>
+<!---->
+<!--                    </div>-->
+<!--                </div>-->
+<!--                --><?php //if($serviceType->view_widget_auto_type == 1 ): ?>
+<!--                    <div class="singleContent__worksWith">-->
+<!--                        <h3>Работаем</h3>-->
+<!--                       <!-- --><?php ///*foreach($autoType as $at):*/?>
+<!--                            <div class="singleContent__worksWith-block">-->
+<!---->
+<!--                                <h4>--><?///*=$at->name*/?><!--</h4>-->
+<!--                                <img src="--><?///*=$at->img_url;*/?><!--" alt="">-->
+<!--                            </div>-->
+<!--                        --><?php ///*endforeach;*/?>
+<!---->
+<!--                        --><?//= AutoType::widget(['view'=>1,'serviceId'=>$serviceID])?>
+<!--                    </div>-->
+<!--                --><?php //endif; ?>
+<!--            </div>-->
+<!--</section>-->
