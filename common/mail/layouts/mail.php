92a38d0fae4c889a1d9335f19bb890b24c5e52b0
king199025 <korol_dima@list.ru>
2016-03-28 14:19:34 +0300

    email

---
+++ common/mail/layouts/mail.php
@@ -0,0 +1,102 @@
+<table width="100%" align="center" cellpadding="0" cellspacing="0" border="0" data-mobile="true" dir="ltr" data-width="700" style="font-size: 16px; background: url(https://multimedia.getresponse.com/106/22105106/photos/349724606.jpg?_ga=1.6473522.1203963895.1458911788) 50% 0% no-repeat rgb(196, 196, 196);">
+    <thead>
+    <tr>
+        <td align="center">
+            <table cellspacing="0" cellpadding="0" align="center" border="0" class="wrapper" width="700" style="width: 700px; margin: auto;">
+                <tbody>
+                <tr>
+                    <td>
+                        <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" data-editable="preheader" data-webinar="0">
+                            <tbody>
+                            <tr>
+                                <td align="center" valign="top" style="padding: 8px; font-family: Helvetica, Arial, sans-serif; color: rgb(38, 38, 38);">
+                                    <br>
+                                    <div><br>
+                                    </div>
+                                    <div><br></div>
+                                    <div><br></div>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+    </thead>
+    <tbody>
+    <tr>
+        <td align="center" valign="top" style="margin: 0; padding: 0;">
+            <table width="700" align="center" border="0" cellspacing="0" cellpadding="0" style="margin: auto; width: 700px; background-image: none; background-color: transparent;" class="wrapper">
+                <tbody>
+                <tr>
+                    <td>
+                        <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                            <tbody>
+                            <tr>
+                                <td valign="top" align="left" style="padding: 0px; margin: 0px;">
+                                    <table cellpadding="0" cellspacing="0" border="0" width="100%">
+                                        <tbody>
+                                        <tr>
+                                            <td style="" align="left" valign="top">
+                                                <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" data-editable="text">
+                                                    <tbody>
+                                                    <tr>
+                                                        <td valign="top" align="left" style="padding: 10px; margin: 0px; font-family: Arial, Helvetica, sans-serif; color: rgb(38, 38, 38); line-height: 2.05; background-color: rgba(15, 15, 15, 0.498039);">
+                                                            <table border="0" cellpadding="0" cellspacing="0" align="left" data-editable="image" style="text-align: right; margin: 0px; padding: 0px;" data-mobile-width="1" id="edi7i0nm" width="154">
+                                                                <tbody>
+                                                                <tr style="text-align: right;">
+                                                                    <td valign="top" align="left" style="display: inline-block; padding: 0px 10px 10px 0px; margin: 0px;" class="tdBlock"><img src="https://multimedia.getresponse.com/106/22105106/photos/349727106.png?_ga=1.17416377.1203963895.1458911788?img1458986009402" width="144" data-origsrc="https://multimedia.getresponse.com/106/22105106/photos/349727106.png?_ga=1.17416377.1203963895.1458911788" data-src="https://multimedia.getresponse.com/106/22105106/photos/349727106.png?_ga=1.17416377.1203963895.1458911788|144|49|144|49|0|0|1" height="49" style="border: 0px none transparent; display: block;"></td>
+                                                                </tr>
+                                                                </tbody>
+                                                            </table>
+                                                            <span style="text-decoration: underline;"><div style="text-align: right;"><span style="font-size: 24px; color: rgb(255, 108, 0); font-family: Tahoma, sans-serif; background-color: inherit;">ПЕРЕЙТИ НА САЙТ</span>
+                                                                </div></span></td>
+                                                    </tr>
+                                                    </tbody>
+                                                </table>
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr>
+                <tr><td style="padding: 0px;"><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" data-editable="line"><tbody><tr><td valign="top" align="center" style="padding: 10px 0px; margin: 0px; background-color: rgb(0, 0, 0);"><div style="height: 1px; line-height: 1px; border-top-width: 2px; border-top-style: solid; border-top-color: rgb(222, 222, 222);"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="" width="1" height="1" style="display:block;"></div></td></tr></tbody></table></td></tr><tr>
+                    <td align="center" valign="top" style="margin: 0; padding: 0;">
+                        <table width="100%" align="center" border="0" cellpadding="0" cellspacing="0" data-editable="text">
+                            <tbody>
+                            <tr>
+                                <td align="center" valign="top" style="margin: 0px; padding: 0px 10px; font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif; color: rgb(255, 255, 255); background-color: rgb(0, 0, 0);"><span style="font-size: 65px;">Текст сообщения</span></td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </td>
+                </tr><tr><td><table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" data-editable="text"><tbody><tr><td align="left" valign="top" style="padding: 10px; font-family: Arial, Helvetica, sans-serif; color: rgb(38, 38, 38); border: 0px none transparent; background-color: rgb(0, 0, 0);"><br><span style="font-family:Arial,Helvetica,sans-serif;color:#262626;font-size:14px"></span><div data-box="button" style="width: 100%; margin-top: 0px; margin-bottom: 0px; text-align: center;"><table border="0" cellpadding="0" cellspacing="0" align="center" data-editable="button" style="padding-bottom: 0px; padding-top: 0px; margin: 0px auto;"><tbody><tr><td valign="top" align="center" class="tdBlock" style="display: inline-block; padding: 13px 25px; margin: 0px; border-radius: 7px; background-color: rgb(255, 108, 0);"><a href="" style="font-family: Arial, 'Helvetica Neue', Helvetica, sans-serif; color: rgb(255, 255, 255); font-size: 18px; text-decoration: none; font-weight: bold;" target="_blank">Активировать аккаунт</a></td></tr></tbody></table></div></td></tr></tbody></table></td></tr>
+
+                <tr><td><table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" data-editable="text"><tbody><tr><td align="left" valign="top" style="padding: 10px; font-family: Arial, Helvetica, sans-serif; color: rgb(38, 38, 38);"><br><span style="font-family:Arial,Helvetica,sans-serif;color:#262626;font-size:14px"></span></td></tr></tbody></table></td></tr><tr><td style="padding: 0px;"><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" data-editable="line"><tbody><tr><td valign="top" align="center" style="padding: 10px 0px; margin: 0px; background-color: rgb(0, 0, 0);"><div style="height:1px;line-height:1px;border-top-width:3px;border-top-style:solid;border-top-color:#dedede;"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="" width="1" height="1" style="display:block;"></div></td></tr></tbody></table></td></tr><tr><td><table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" data-editable="preheader" data-webinar="0"><tbody><tr><td align="center" valign="top" style="padding: 8px; font-family: Helvetica, Arial, sans-serif; color: rgb(38, 38, 38); font-size: 24px; background-color: rgb(0, 0, 0);"><div style="text-align: left;"><span style="color: rgb(255, 255, 255); background-color: transparent;"><font size="26" style="font-size: 26px;">Наши контакты</font></span></div><div style="text-align: left;"><br></div><div style="text-align: left;"><span style="color: rgb(255, 255, 255);"><font size="17" style="font-size: 17px;">carbax@mail.ru</font></span></div><div style="text-align: left;"><span style="color: rgb(255, 255, 255);"><font size="17" style="font-size: 17px;">555555555555</font></span></div></td></tr></tbody></table></td></tr>
+
+
+
+
+
+
+                <tr><td style="padding: 0px;"><table border="0" cellpadding="0" cellspacing="0" align="center" width="100%" data-editable="line"><tbody><tr><td valign="top" align="center" style="padding: 10px 0px; margin: 0px; background-color: rgb(0, 0, 0);"><div style="height:1px;line-height:1px;border-top-width:3px;border-top-style:solid;border-top-color:#dedede;"><img src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt="" width="1" height="1" style="display:block;"></div></td></tr></tbody></table></td></tr><tr><td><table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" data-editable="preheader" data-webinar="0"><tbody><tr><td align="center" valign="top" style="padding: 8px; font-family: Helvetica, Arial, sans-serif; color: rgb(255, 255, 255); background-color: rgb(0, 0, 0);"><span style="color: rgb(255, 255, 255);">© 2016 by Carbax.ru. All Rights Reserved.<span style="font-family: Helvetica, Arial, sans-serif; font-size: 12px;">.</span></span></td></tr></tbody></table></td></tr></tbody>
+            </table>
+        </td>
+    </tr>
+    </tbody>
+</table>
+
+
+
+
+
+
+
+
