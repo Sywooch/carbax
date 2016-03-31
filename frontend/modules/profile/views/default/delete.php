efe958d5bd66515ebf78e0df1abbc8326704cc60
king199025 <korol_dima@list.ru>
2016-03-31 15:30:25 +0300

    view del profile

--- frontend/modules/profile/views/default/delete.php
+++ frontend/modules/profile/views/default/delete.php
@@ -1,13 +1,18 @@
 <?php
+use yii\helpers\Html;
+
 $this->title = 'Удалить профиль?';
 $this->params['breadcrumbs'][] = ['label' => 'Профиль', 'url' => ['/profile/default/view']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <section class="main-container" >
+        <p class="text-center">
+                <?= Html::img('/media/img/delprofile.png'); ?>
+        </p>
+        <p class="regInfo"><span class="regInfoOrange">Все ваши данные а так же данные о бизнесе будут удалены!!!</span></p>
+        <p class="regInfo"><span class="regInfoOrange">Вы действительно хотите удалить профиль?</span></p>
 
-        Вы действительно хотите удалить профиль?
-        Все ваши данные а так же данные о бизнесе будут удалены!!!
-        <?= \yii\helpers\Html::a('Да', \yii\helpers\Url::to('/profile/default/del_prof')); ?>
-        <?= \yii\helpers\Html::a('Нет', \yii\helpers\Url::to('/profile/default/view')); ?>
+        <p class="regInfo"> <?= Html::a('Да', \yii\helpers\Url::to('/profile/default/del_prof'),['class'=>'btn btn-danger']); ?>
+        <?= Html::a('Нет', \yii\helpers\Url::to('/profile/default/view'),['class'=>'btn btn-success']); ?></p>
 
 </section>
